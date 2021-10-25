20fc152e6fbe89d97d83f228cdc7d563c45c6ec7
Amy H <69822551+amyh25@users.noreply.github.com>
2021-10-25 12:19:52 -0400

    Initial commit

---
+++ R/example.R
@@ -0,0 +1,13 @@
+#' Function name
+#' 
+#' Function description
+#' 
+#' @param name string
+#' @return A hello world string
+#' @export
+
+hello_world <- function(name) {
+  paste0("Hello ", name, "!")
+}
+
+# then run devtools::document()
