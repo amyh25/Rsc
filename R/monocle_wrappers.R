8276e5d9492e9fb886c423dccf9d5540e8ce0fbd
Amy H <69822551+amyh25@users.noreply.github.com>
2021-11-22 11:20:10 -0500

    fixing monocle wrappers again

--- R/monocle_wrappers.R
+++ R/monocle_wrappers.R
@@ -1,25 +1,9 @@
-#' set_idents
-#' 
-#' Sets identity of Seurat object
-#' Equivalent to Ident(so) <- "ident"
-#' 
-#' @param so Seurat object
-#' @param ident Identity given as string
-#' @return Seurat object with identity set to given ident
-#' @export
-
-set_idents <- function(so, ident) {
-  Idents(so) <- ident
-  return(so)
-}
-
-
 #' run_monocle_trajectory
 #' 
 #' Wrapper for the monocle pipeline: 
 #' so %>% as.cell_data_set() %>% cluster_cells() %>% learn_graph() %>% order_cells()
 #' 
-#' @param so
+#' @param so seurat object
 #' @return cell data set object
 #' @export
 
