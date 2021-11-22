61d6a662c11f13bd187df6c8de7a69b115f7fcd0
Amy H <69822551+amyh25@users.noreply.github.com>
2021-11-22 12:01:35 -0500

    adding monocle function dependencies'

--- R/monocle_wrappers.R
+++ R/monocle_wrappers.R
@@ -8,11 +8,26 @@
 #' @export
 
 run_monocle_trajectory <- function(
-  so, reduction_method = "UMAP"
+  so, reduction_method = "UMAP", 
+  use_partition = FALSE, close_loop = FALSE
 ) {
   so %>% 
-    as.cell_data_set %>% 
+    as.cell_data_set() %>% 
     cluster_cells(reduction_method = reduction_method) %>% 
-    learn_graph() %>% 
+    learn_graph(use_parition = use_partition, 
+                close_loop = close_loop) %>% 
     order_cells(reduction_method = reduction_method)
+}
+
+#' add_pseudotime_metadata
+#' 
+#' Adds pseudotime as metadata to seurat object
+#' 
+#' @param so seurat object
+#' @param cds cell data set object
+#' @return seurat object with pseudotime metadata
+#' @export 
+
+add_pseudotime_metadata <- function(so, cds) {
+  as.data.frame(pseudotime(cds))
 }
