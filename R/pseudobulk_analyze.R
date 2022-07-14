cf7ed2f88c84b4c18caf95a88564a7e2a40aa782
Amy H <69822551+amyh25@users.noreply.github.com>
2022-07-14 16:33:56 -0400

    added package specifications

--- R/pseudobulk_analyze.R
+++ R/pseudobulk_analyze.R
@@ -11,13 +11,13 @@
 pb_to_deseq <- function(pb, sample_var, vars, 
                         formula_str = paste0("~", paste0(vars, collapse = "+"))) {
   counts_mtx <- as.matrix(pb)
-  counts_metadata <- tibble(id = rownames(pb)) %>% 
-    separate(id, into = c(sample_var, vars), sep = "_")
+  counts_metadata <- tibble::tibble(id = rownames(pb)) %>% 
+    tidyr::separate(id, into = c(sample_var, vars), sep = "_")
   de <- DESeqDataSetFromMatrix(
-    t(counts_mtx), 
+    Matrix::t(counts_mtx), 
     colData = counts_metadata, 
     design = as.formula(formula_str)
   )
-  de <- DESeq(de)
+  de <- DESeq2::DESeq(de)
 }
 
